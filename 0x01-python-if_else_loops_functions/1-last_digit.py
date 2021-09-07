61959ecb31c15e9fd9e83b0a715b8531082fd8f2
sammysamp <sammysamp@gmail.com>
2021-09-07 14:05:16 -0700

    using if and mod for print las digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    lastdigit = 10 - number % 10
+    lastdigit = number % 10 - 10
 else:
     lastdigit = number % 10
 if lastdigit == 0:
