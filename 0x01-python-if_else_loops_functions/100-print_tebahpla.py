9a8779510442675f093b44d0395fcb1aea8acce2
sammysamp <sammysamp@gmail.com>
2021-09-08 19:35:11 -0700

    printing reverse alphabet with for -1

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+min, max = ord('a') - 1, ord('z')
+for i in range(max, min, -1):
+    if i % 2 == 0:
+        print("{:c}".format(i), end="")
+    else
+        print("{:c}".format(i - 32), end="")
