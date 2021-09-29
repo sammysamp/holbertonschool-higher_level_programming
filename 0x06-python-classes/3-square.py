8d56f6449e16a68c0baa2b5e895fe15314eba6ff
sammysamp <sammysamp@gmail.com>
2021-09-29 12:07:42 -0700

    using second method in a class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""In this module we defiine a class for a square object"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """Initializes an object for a square class.
+        Args:
+        size (int): The size of the square. Can be 0 if size is not
+        specified.
+        """
+        if not(type(size) is int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area.
+        Returns:
+            The area of a square
+        """
+        return self.__size ** 2
