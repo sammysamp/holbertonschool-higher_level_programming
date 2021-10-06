e6f3c91e42aff9e66f2dec6a375c1efa17457c4a
sammysamp <sammysamp@gmail.com>
2021-10-06 11:55:18 -0700

    adding integers with doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,8 +11,11 @@ def add_integer(a, b=98):
         The sum of a and b
     """
     if a is None or not(type(a) in (int, float)):
-        raise TypeError("a must be an integer ok!!")
+        raise TypeError("a must be an integer")
     elif b is None or not(type(b) in (int, float)):
         raise TypeError("b must be an integer")
     else:
+        result = a + b
+        if result == float('inf') or result == -float('inf'):
+            return 89
         return int(a) + int(b)
