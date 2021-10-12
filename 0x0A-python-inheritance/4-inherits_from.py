68507b1949aa5a0ce4655048f4968a81682e882c
sammysamp <sammysamp@gmail.com>
2021-10-11 20:50:22 -0700

    classes and subclasses

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class ;
+    otherwise False
+    """
+    if a_class == type(obj):
+        return False
+    return isinstance(obj, a_class)
