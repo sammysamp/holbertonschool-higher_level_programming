862ce5b7c55457a75b2790fabf409672117e6462
sammysamp <sammysamp@gmail.com>
2021-10-12 19:50:00 -0700

    reading file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """ Reading a text file """
 
 def read_file(filename=""):
+    """read file method"""
     with open('my_file_0.txt', encoding='utf-8') as f:
         read_data = f.read()
     print(read_data)
