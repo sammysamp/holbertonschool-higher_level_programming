540fdac563ffbf5623117e8460dda80a4af4131e
sammysamp <sammysamp@gmail.com>
2021-10-12 20:03:12 -0700

    writing file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """method"""
 
+
 def write_file(filename="", text=""):
     """write on file create the file if doesn’t exist
-    overwrite the content of the file if it already exists"""    
+    overwrite the content of the file if it already exists"""
     with open(filename, mode='w', encoding='UTF-8') as f:
         counter = f.write(text)
     return(counter)
