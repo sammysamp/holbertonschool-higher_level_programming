bd21b411a18aa4f68c42b17558de72b54b67eed9
sammysamp <sammysamp@gmail.com>
2021-10-18 16:03:33 -0700

    Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,8 +11,6 @@ class Rectangle(Base):
         """initialize method"""
         self.__width = width
         self.__height = height
-        self.x = x
-        self.y = y
         super().__init__(id)
 
     @property
@@ -21,10 +19,6 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, new_width):
-        if type(new_width) is not int:
-            raise TypeError("width must be an integer")
-        if new_width <= 0:
-            raise ValueError("width must be > 0")
         self.__width = new_width
 
     @property
@@ -33,11 +27,6 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, new_height):
-        if type(new_height) is not int:
-            raise TypeError("height must be an integer")
-        if new_height <= 0:
-            raise ValueError("height must be > 0")
-
         self.__height = new_height
 
     @property
