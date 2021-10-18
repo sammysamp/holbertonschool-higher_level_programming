8255a38276e9ce4ec17efec4baa1941402bac976
sammysamp <sammysamp@gmail.com>
2021-10-18 12:43:10 -0700

    inserting node on a ordered singled linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *curr, *prev = NULL, *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL || (*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (curr = *head; curr != NULL && curr->n < number; curr = curr->next)
+		prev = curr;
+	new->next = prev->next;
+	prev->next = new;
+	return (new);
+}
