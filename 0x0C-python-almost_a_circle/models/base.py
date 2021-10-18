4f278f9cb53e935b05a601a567128d562414855e
sammysamp <sammysamp@gmail.com>
2021-10-18 15:25:05 -0700

    creating Python package

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class"""
+
+
+import json
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
