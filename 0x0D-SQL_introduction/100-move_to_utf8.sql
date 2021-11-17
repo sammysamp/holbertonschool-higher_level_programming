8790b39df88693880b8c8004d78f4cc3b74f2a05
sammysamp <sammysamp@gmail.com>
2021-11-17 06:49:59 -0800

    move to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,7 +9,7 @@ ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
---ALTER TABLE first_table
---MODIFY name VARCHAR(256)
---COLLATE utf8mb4_unicode_ci
---NULL;
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+COLLATE utf8mb4_unicode_ci
+NULL;
