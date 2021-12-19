90ebf1ea1e49ff90e24c3e2627e9a2d903a3ca54
sammysamp <sammysamp@gmail.com>
2021-12-19 08:04:38 -0800

    using not in, and JOIN instead double select

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- inner join
+SELECT DISTINCT a.name
+FROM tv_genres a
+INNER JOIN tv_show_genres b
+on a.id = b.genre_id
+WHERE b.genre_id NOT IN (
+      		     	SELECT b.genre_id
+			 FROM tv_shows a
+			 INNER JOIN tv_show_genres b
+			 ON a.id = b.show_id
+			 WHERE a.title = 'Dexter')
+ORDER BY 1;
