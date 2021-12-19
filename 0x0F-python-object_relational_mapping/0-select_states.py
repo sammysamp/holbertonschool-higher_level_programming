163b1226b84702a5aa51a4ce2ff4137ee2b496fe
sammysamp <sammysamp@gmail.com>
2021-12-19 08:59:09 -0800

    connecting database and using cursor

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnnect():
+    """
+    Connecting and querying database
+    """
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], passwd=argv[2],
+                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("can't connect to database")
+        return 0
+    cur = db_connection.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_connection.close()
+
+
+mysqlconnnect()
