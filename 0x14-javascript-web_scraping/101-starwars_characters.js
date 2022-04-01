c07ea964622934b4fc432e9b6f21c75f2f6d1ef0
sammysamp <sammysamp@gmail.com>
2022-04-01 12:36:10 -0700

    fix file permissions

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,29 @@
 #!/usr/bin/node
-
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, { json: true }, function (error, res, body) {
+request(url, function (error, response, body) {
   if (error) {
-    return console.log(error);
-  }
-  for (const i of body.characters) {
-    request(i, { json: true }, function (error, res, body) {
-      if (error) {
-        return console.log(error);
-      }
-      console.log(body.name);
-    });
+    console.error('error:', error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    const length = characterList.length;
+    CharacterRequest(0, characterList[0], characterList, length);
   }
 });
+
+function CharacterRequest (idx, urlChar, characters, limit) {
+  if (idx === limit) { return; }
+  request(urlChar, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      CharacterRequest(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
+  });
+}
